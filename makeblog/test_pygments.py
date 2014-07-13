9452b0c69de8e9bfa0b84dc362363d98265dc8ef
Stefan J. Betz <info@stefan-betz.net>
2014-07-13 17:39:01 +0200

    Full coverage for pygments.

--- makeblog/test_pygments.py
+++ makeblog/test_pygments.py
@@ -15,3 +15,37 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from unittest import TestCase
 from makeblog.pygments import *
+
+CONTENT_WITHOUT_CODE = """
+This is content
+without any code.
+"""
+
+CONTENT_WIHT_CODE = """
+This is content
+$$code(lang=sh)
+#!/bin/bash
+echo "bla"
+$$/code
+with code in it.
+"""
+
+CONTENT_WITH_MISSING_LEXER = """
+This content has a missing lexer
+$$code(lang=nerverexisting)
+bla
+$$/code
+"""
+
+
+class TestPygments(TestCase):
+
+	def test_pygmentify(self):
+
+		@pygmentify
+		def render(content):
+			return content
+
+		self.assertIsInstance(render(CONTENT_WIHT_CODE),str)
+		self.assertIsInstance(render(CONTENT_WITHOUT_CODE),str)
+		self.assertIsInstance(render(CONTENT_WITH_MISSING_LEXER),str)
