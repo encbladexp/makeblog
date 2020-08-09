913f7b8798d6069c5cae04d41379d936704b7078
Stefan J. Betz <info@stefan-betz.net>
2020-08-09 14:03:44 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name="makeblog",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple offline Blog.",
     author="Stefan J. Betz",
     author_email="info@stefan-betz.net",
